e914f4dc4f35a2ba805e55759cf368f21bd85e60
Leonel <mendiolaleonel2002@gmail.com>
2026-09-14 19:57:58 -0600

    Funciones generator

---
+++ generator/password_generator.py
@@ -0,0 +1,25 @@
+import string
+import random 
+
+def generator_password(length = 12):
+    characters = (
+        string.ascii_lowrercase +
+        string.ascii_uppercase + 
+        string.digits +
+        string.punctuation
+    )
+
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
+
+def has_lowercase(password):
+    return any(c.islower() for c in password)
+
+def has_uppercase(password):
+    return any(c.isupper() for c in password)
+
+def has_number(password):
+    return any(c.isdigit() for c in password)
+
+def has_symbol(password):
+    return any(c in string.punctuation for c in password)
